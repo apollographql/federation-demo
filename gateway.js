f96214fb4299c8be0eff7a7b99ce2f52bf9b20d8
James Baxley <james@apollographql.com>
2019-05-24 00:36:48 -0400

    initial demo

---
+++ gateway.js
@@ -0,0 +1,28 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const serviceList = [
+  { name: "accounts", url: "http://localhost:4001/graphql" },
+  { name: "reviews", url: "http://localhost:4002/graphql" },
+  { name: "products", url: "http://localhost:4003/graphql" },
+  { name: "inventory", url: "http://localhost:4004/graphql" }
+];
+
+const gateway = new ApolloGateway({ serviceList });
+
+(async () => {
+  const { schema, executor } = await gateway.load();
+
+  const server = new ApolloServer({
+    schema,
+    executor,
+    playground: {
+      cdnUrl: "//cdn.jsdelivr.net/npm/@apollographql",
+      version: "beta"
+    }
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})();
