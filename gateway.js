5ef95cc15cb48eeb174752e90dd23a6c2dc5ba89
Solomon White <rubysolo@gmail.com>
2019-07-02 09:52:36 -0600

    fix references to child services
    I had to make this change to run the demo locally.

--- gateway.js
+++ gateway.js
@@ -3,10 +3,10 @@ const { ApolloGateway } = require("@apollo/gateway");
 
 const gateway = new ApolloGateway({
   serviceList: [
-    { name: "accounts", url: "http://localhost:4001/graphql" },
-    { name: "reviews", url: "http://localhost:4002/graphql" },
-    { name: "products", url: "http://localhost:4003/graphql" },
-    { name: "inventory", url: "http://localhost:4004/graphql" }
+    { name: "accounts", url: "http://localhost:4001" },
+    { name: "reviews", url: "http://localhost:4002" },
+    { name: "products", url: "http://localhost:4003" },
+    { name: "inventory", url: "http://localhost:4004" }
   ]
 });
 
