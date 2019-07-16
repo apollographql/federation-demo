dbca51ddfa13e073489a60e514a480b02170e503
Adam Zionts <adam@meteor.com>
2019-07-16 11:11:26 -0700

    Migrate to managed federation
    Before doing this it is essential to register all of the services
    individually. See
    https://www.apollographql.com/docs/platform/federation/#registering-federated-services
    for more detailed information about this workflow.
    
      The following commands were all run, subsequent to `npm
      start-services` and after ensuring that a `.env` file is defined with
      a valid `ENGINE_API_KEY`.
    
      npx apollo service:push --endpoint http://localhost:4001/ --serviceURL
      http://localhost:4001/ --serviceName accounts --tag local
    
      npx apollo service:push --endpoint http://localhost:4002/ --serviceURL
      http://localhost:4002/ --serviceName inventory --tag local
    
      npx apollo service:push --endpoint http://localhost:4003/ --serviceURL
      http://localhost:4003/ --serviceName products --tag local
    
      npx apollo service:push --endpoing http://localhost:4004/ --serviceURL
      http://localhost:4004/ --serviceName reviews --tag local
    
      You can verify that services are registered as expected using `npx
      apollo service:list --tag local` and you should see all of the
      federated services appear, along with their URLs!

--- gateway.js
+++ gateway.js
@@ -1,21 +1,12 @@
-const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
 
-const gateway = new ApolloGateway({
-  serviceList: [
-    { name: "accounts", url: "http://localhost:4001/graphql" },
-    { name: "reviews", url: "http://localhost:4002/graphql" },
-    { name: "products", url: "http://localhost:4003/graphql" },
-    { name: "inventory", url: "http://localhost:4004/graphql" }
-  ]
+// Managed configuration will be picked up using the API key
+const gateway = new ApolloGateway();
+
+// Pass the gateway to Apollo Server
+const server = new ApolloServer({ gateway, subscriptions: false });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
-
-(async () => {
-  const { schema, executor } = await gateway.load();
-
-  const server = new ApolloServer({ schema, executor });
-
-  server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-})();
