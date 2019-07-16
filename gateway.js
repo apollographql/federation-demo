6b83f53d9cb8ee5f4c5cc6e69721c3e4fb9c58b3
Adam Zionts <adam@meteor.com>
2019-07-16 11:24:50 -0700

    Add debug logging to the gateway
    This prints out log messages as managed configuration changes
    
    for instance:
    
    [DEBUG] Tue Jul 16 2019 11:22:22 GMT-0700 (Pacific Daylight Time) apollo-gateway: Loading configuration for Gateway
    🚀 Server ready at http://localhost:4000/
    [DEBUG] Tue Jul 16 2019 11:22:22 GMT-0700 (Pacific Daylight Time) apollo-gateway: Configuration loaded for Gateway
    [DEBUG] Tue Jul 16 2019 11:22:22 GMT-0700 (Pacific Daylight Time) apollo-gateway: Composing schema from service list:
      http://localhost:4001/: accounts
      http://localhost:4002/: reviews
      http://localhost:4003/: products
      http://localhost:4004/: inventory
    [DEBUG] Tue Jul 16 2019 11:22:22 GMT-0700 (Pacific Daylight Time) apollo-gateway: Schema loaded and ready for execution

--- gateway.js
+++ gateway.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const { ApolloGateway } = require('@apollo/gateway');
 
 // Managed configuration will be picked up using the API key
-const gateway = new ApolloGateway();
+const gateway = new ApolloGateway({debug: true});
 
 // Pass the gateway to Apollo Server
 const server = new ApolloServer({ gateway, subscriptions: false });
