a2cf4fe4303281b569c2b76f76dff4a72b88fe8f
David Christie <davidchristie.nz@gmail.com>
2019-09-29 17:53:37 +0200

    Add unit tests for createUser function

---
+++ services/accounts/source/repositories/users.test.ts
@@ -0,0 +1,51 @@
+import faker from "faker";
+import { User } from "../types";
+import { createUser } from "./users";
+import { Driver } from "neo4j-driver/types/v1";
+
+let driver: Driver;
+
+jest.mock("../neo4j", () => {
+  return {
+    createDriver: () => {
+      driver = {
+        close: jest.fn(),
+        session: jest.fn()
+      };
+      return driver;
+    }
+  };
+});
+
+describe("createUser function", () => {
+  let session: any;
+  let name: string;
+  let email: string;
+  let password: string;
+  let user: User;
+
+  beforeEach(async () => {
+    name = faker.name.findName();
+    email = faker.internet.email();
+    password = faker.internet.password();
+    session = {
+      close: jest.fn(),
+      run: jest.fn()
+    };
+    (driver.session as any).mockReturnValueOnce(session);
+    user = await createUser({ email, name, password });
+  });
+
+  it("returns a user", async () => {
+    expect(user).toEqual({
+      email,
+      id: expect.any(String),
+      name,
+      password: expect.any(String)
+    });
+  });
+
+  it("closes the session", () => {
+    expect(session.close).toHaveBeenCalledTimes(1);
+  });
+});
--- services/accounts/source/repositories/users.ts
+++ services/accounts/source/repositories/users.ts
@@ -1,12 +1,9 @@
-import neo4j from "neo4j-driver";
 import uuid from "uuid/v4";
+import { createDriver } from "../neo4j";
 import { hashPassword } from "../passwords";
 import { User } from "../types";
 
-const url = process.env.NEO4J_HOST || "bolt://accounts-neo4j:7687";
-const user = process.env.NEO4J_USER || "neoj4";
-const password = process.env.NEO4J_PASSWORD || "accounts";
-const driver = neo4j.driver(url, neo4j.auth.basic(user, password));
+const driver = createDriver();
 
 export async function createUser({ email, name, password }): Promise<User> {
   const id = uuid();
