b5897dd1415849bb7841597101188830c4fba976
Vijay Manchi <vijayman@microsoft.com>
2020-09-03 01:15:32 -0700

    Investigate single field cases

--- services/accounts/index.js
+++ services/accounts/index.js
@@ -13,6 +13,7 @@ const typeDefs = gql`
     id: ID!
     name: String
     username: String
+    reviews: [Review]
   }
 
   type Product @key(fields: "upc") {
@@ -20,6 +21,15 @@ const typeDefs = gql`
     name: String
     price: Int
     weight: Int
+    reviews: [Review]
+    shippingEstimate: Int
+  }
+
+  type Review @key(fields: "id") {
+    id: ID!
+    body: String
+    author: User
+    product: Product
   }
 `;
 
@@ -35,11 +45,62 @@ const resolvers = {
   User: {
     __resolveReference(object) {
       return users.find(user => user.id === object.id);
+    },
+    reviews(user) {
+      return reviews.filter(review => review.authorID === user.id);
+    },
+    numberOfReviews(user) {
+      return reviews.filter(review => review.authorID === user.id).length;
+    },
+    username(object) {
+      const found = users.find(user => user.id === object.id);
+      return found? found.username : null;
+    },
+    name(object) {
+      const found = users.find(user => user.id === object.id);
+      return found? found.name : null;
     }
   },
   Product: {
     __resolveReference(object) {
       return products.find(product => product.upc === object.upc);
+    },
+    reviews(product) {
+      return reviews.filter(review => review.product.upc === product.upc);
+    },
+    shippingEstimate(object) {
+      const found = products.find(product => product.upc === object.upc)
+      // free for expense items
+      if (!found || found.price > 1000) return 0;
+      // estimate is based on weight
+      return found.weight * 0.5;
+    },
+    name(object) {
+      const found = products.find(product => product.upc == object.upc)
+      return found? found.name : null;
+    },
+    price(object) {
+      const found = products.find(product => product.upc == object.upc)
+      return found? found.price : null;
+    },
+    weight(object) {
+      const found = products.find(product => product.upc == object.upc)
+      return found? found.weight : null;
+    }
+  },
+  Review: {
+    __resolveReference(object) {
+      return reviews.find(review => review.id === object.id);
+    },
+    author(review) {
+      return { __typename: "User", id: review.authorID };
+    },
+    product(object) {
+      const found = reviews.find(review => review.id === object.id);
+      const result = found
+        ? { __typename: "Product", upc: found.product.upc } 
+        : null;
+      return result;
     }
   }
 };
@@ -52,7 +113,7 @@ const server = new ApolloServer({
     }
   ]),
   formatResponse: (response, requestContext) => {
-    console.log(`Accounts:Sending response - ${counter++}`);
+    console.log(`Accounts:Sending response - ${++counter}`);
     // console.log(requestContext);
   }
 });
@@ -94,5 +155,32 @@ const products = [
     name: "Chair",
     price: 54,
     weight: 50
+  }
+];
+
+const reviews = [
+  {
+    id: "1",
+    authorID: "1",
+    product: { upc: "1" },
+    body: "Love it!"
+  },
+  {
+    id: "2",
+    authorID: "1",
+    product: { upc: "2" },
+    body: "Too expensive."
+  },
+  {
+    id: "3",
+    authorID: "2",
+    product: { upc: "3" },
+    body: "Could be better."
+  },
+  {
+    id: "4",
+    authorID: "2",
+    product: { upc: "1" },
+    body: "Prefer something else."
   }
 ];
--- services/reviews/index.js
+++ services/reviews/index.js
@@ -4,62 +4,19 @@ const { buildFederatedSchema } = require("@apollo/federation");
 var counter = 0;
 
 const typeDefs = gql`
-  type Review @key(fields: "id") {
-    id: ID!
-    body: String
-    author: User @provides(fields: "username")
-    product: Product
-  }
-
-  extend type User @key(fields: "id") {
-    id: ID! @external
-    username: String @external
-    reviews: [Review]
-  }
-
   extend type Product @key(fields: "upc") {
     upc: String! @external
-    reviews: [Review]
-    weight: Int @external
-    price: Int @external
     inStock: Boolean
-    shippingEstimate: Int @requires(fields: "price weight")
   }
 `;
 
 const resolvers = {
-  Review: {
-    author(review) {
-      return { __typename: "User", id: review.authorID };
-    }
-  },
-  User: {
-    reviews(user) {
-      return reviews.filter(review => review.authorID === user.id);
-    },
-    numberOfReviews(user) {
-      return reviews.filter(review => review.authorID === user.id).length;
-    },
-    username(user) {
-      const found = usernames.find(username => username.id === user.id);
-      return found ? found.username : null;
-    }
-  },
   Product: {
     __resolverReference(object) {
       return {
         ...object,
         ...inventory.find(product => product.upc === object.upc)
       }
-    },
-    reviews(product) {
-      return reviews.filter(review => review.product.upc === product.upc);
-    },
-    shippingEstimate(object) {
-      // free for expense items
-      if (object.price > 1000) return 0;
-      // estimate is based on weight
-      return object.weight * 0.5;
     },
     inStock(object) {
       const found = inventory.find(product => product.upc === object.upc)
@@ -76,7 +33,7 @@ const server = new ApolloServer({
     }
   ]),
   formatResponse: (response, requestContext) => {
-    console.log(`Reviews:Sending response - ${counter++}`);
+    console.log(`Reviews:Sending response - ${++counter}`);
     // console.log(requestContext);
   }
 });
@@ -84,37 +41,6 @@ const server = new ApolloServer({
 server.listen({ port: 4002 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-
-const usernames = [
-  { id: "1", username: "@ada" },
-  { id: "2", username: "@complete" }
-];
-const reviews = [
-  {
-    id: "1",
-    authorID: "1",
-    product: { upc: "1" },
-    body: "Love it!"
-  },
-  {
-    id: "2",
-    authorID: "1",
-    product: { upc: "2" },
-    body: "Too expensive."
-  },
-  {
-    id: "3",
-    authorID: "2",
-    product: { upc: "3" },
-    body: "Could be better."
-  },
-  {
-    id: "4",
-    authorID: "2",
-    product: { upc: "1" },
-    body: "Prefer something else."
-  }
-];
 
 const inventory = [
   { upc: "1", inStock: true },
