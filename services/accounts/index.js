6069a9159e2fe807066319caa5223bd7b5717673
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2021-05-05 18:14:25 -0300

    💥 illustrate graphql-middleware issue

--- services/accounts/index.js
+++ services/accounts/index.js
@@ -1,5 +1,7 @@
-const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
+const { ApolloServer, gql } = require("apollo-server");
+const { applyMiddleware } = require('graphql-middleware')
+const assert = require("assert")
 
 const typeDefs = gql`
   extend type Query {
@@ -26,14 +28,32 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({
-  schema: buildFederatedSchema([
-    {
-      typeDefs,
-      resolvers
-    }
-  ])
-});
+const schema = buildFederatedSchema([{ typeDefs, resolvers }]);
+
+// assertion passes
+assert.strictEqual(
+  schema.getType("User").resolveReference,
+  resolvers.User.__resolveReference
+);
+
+const schemaWithMiddleware = applyMiddleware(
+  schema,
+  (resolve, root, args, context, info) => resolve(root, args, context, info)
+);
+
+// assertion fails:
+//
+// [start-service-accounts] AssertionError [ERR_ASSERTION]: Expected values to be strictly equal:
+// [start-service-accounts] + actual - expected
+// [start-service-accounts]
+// [start-service-accounts] + undefined
+// [start-service-accounts] - [Function: __resolveReference]
+assert.strictEqual(
+  schemaWithMiddleware.getType("User").resolveReference,
+  resolvers.User.__resolveReference
+);
+
+const server = new ApolloServer({ schema: schemaWithMiddleware });
 
 server.listen({ port: 4001 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
