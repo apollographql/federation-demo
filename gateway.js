66a758b17bdac576bef2b29530ee5f145f582680
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-08-05 17:36:55 +0100

    Delay gateway startup

--- gateway.js
+++ gateway.js
@@ -18,20 +18,22 @@ const gateway = new ApolloGateway({
   __exposeQueryPlanExperimental: false,
 });
 
-(async () => {
-  const server = new ApolloServer({
-    gateway,
-
-    // Apollo Graph Manager (previously known as Apollo Engine)
-    // When enabled and an `ENGINE_API_KEY` is set in the environment,
-    // provides metrics, schema management and trace reporting.
-    engine: false,
-
-    // Subscriptions are unsupported but planned for a future Gateway version.
-    subscriptions: false,
-  });
-
-  server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-})();
+setTimeout(() => {
+  (async () => {
+    const server = new ApolloServer({
+      gateway,
+  
+      // Apollo Graph Manager (previously known as Apollo Engine)
+      // When enabled and an `ENGINE_API_KEY` is set in the environment,
+      // provides metrics, schema management and trace reporting.
+      engine: false,
+  
+      // Subscriptions are unsupported but planned for a future Gateway version.
+      subscriptions: false,
+    });
+  
+    server.listen().then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+    });
+  })();
+}, 2000);
