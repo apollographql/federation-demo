954afdbdb25cc8a844703319a3676f7538150b3f
o0Ignition0o <jeremy.lempereur@gmail.com>
2022-10-19 12:59:46 +0200

    update dependencies, and use IntrospectAndCompose instead of a serviceList

--- gateway.js
+++ gateway.js
@@ -1,19 +1,22 @@
 const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 
-const gateway = new ApolloGateway({
+const supergraphSdl = new IntrospectAndCompose({
   // This entire `serviceList` is optional when running in managed federation
   // mode, using Apollo Graph Manager as the source of truth.  In production,
   // using a single source of truth to compose a schema is recommended and
   // prevents composition failures at runtime using schema validation using
   // real usage-based metrics.
-  serviceList: [
+  subgraphs: [
     { name: "accounts", url: "http://localhost:4001/graphql" },
     { name: "reviews", url: "http://localhost:4002/graphql" },
     { name: "products", url: "http://localhost:4003/graphql" },
-    { name: "inventory", url: "http://localhost:4004/graphql" }
+    { name: "inventory", url: "http://localhost:4004/graphql" },
   ],
+});
 
+const gateway = new ApolloGateway({
+  supergraphSdl,
   // Experimental: Enabling this enables the query plan view in Playground.
   __exposeQueryPlanExperimental: false,
 });
